29378eb4f0e16a1ff3ea013fe6ae45d87a1e10d8
shahspider@github.com <aq.syed007@gmail.com>
2025-11-26 23:36:53 +0500

    add websocket url env var VITE_API_URL

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -50,8 +50,9 @@ const Conversation = ({ conversationId, currentUserId, onBack }) => {
     if (!conversationId) return;
     const token = localStorage.getItem('access_token');
 
-    const websocket = new WebSocket(`ws://localhost:8000/ws/chat/${conversationId}/?token=${token}`);
-    console.log(`WebSocket URL: ws://localhost:8000/ws/chat/${conversationId}/?token=${token}`);
+    const websocket = new WebSocket(
+      `${import.meta.env.VITE_WS_BASE_URL}/ws/chat/${conversationId}/?token=${token}`
+    );
     websocket.onopen = () => {
       console.log("WebSocket connection established");
     };
